d2423461fa2546bc4545fa0041c4e245fdd5ac13
HAHC21 <hahc21@gmail.com>
2021-10-04 11:06:34 -0500

    updates

--- app.py
+++ app.py
@@ -29,17 +29,22 @@ songs_filtered = []
 songs_artists_filtered = []
 songs_albums_filtered = []
 
+albums_filter_filtered = []
+artists_filter_filtered = []
+
 # Main route
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
 
+        # Get search parameter
         title = request.form.get('title')
 
+        # URL for Unofficial Spotify API
         url = "https://unsa-unofficial-spotify-api.p.rapidapi.com/search"
 
+        # API Parameters ahd headers
         querystring = {"query":f"{title}","count":"2", "type":"tracks"}
-
         headers = {
             'x-rapidapi-host': "unsa-unofficial-spotify-api.p.rapidapi.com",
             'x-rapidapi-key': "4ce7fb66fbmsh75f27b6cb5e1416p13155djsn7869b3c397e0"
@@ -49,8 +54,10 @@ def index():
 
         songs_response = json.loads(response.text)
 
+        # Extract the Results List from the overall response
         songs_data = songs_response["Results"]
 
+        # Clear the global lists to make space for the new search
         songs_filtered.clear()
         albums_filter.clear()
         artists_filter.clear()
@@ -61,8 +68,10 @@ def index():
             album_cover = song["album"]["images"][0]["url"]
             artist_name = song["album"]["artists"][0]["name"]
             spotify_link = song["external_urls"]["spotify"]
+            preview_url = song["preview_url"]
+            song_uri = song["uri"]
 
-            songs_filtered.append({"song_name":f"{song_name}", "album_name":f"{album_name}", "album_cover":f"{album_cover}", "artist_name":f"{artist_name}", "spotify_link":f"{spotify_link}"})
+            songs_filtered.append({"song_name":f"{song_name}", "album_name":f"{album_name}", "album_cover":f"{album_cover}", "artist_name":f"{artist_name}", "spotify_link":f"{spotify_link}", "preview_url": f"{preview_url}", "uri": f"{song_uri}"})
             
             if album_name in albums_filter:
                 pass
@@ -74,7 +83,7 @@ def index():
             else:
                 artists_filter.append(artist_name)
 
-        return render_template("results.html", title = title, songs=songs_filtered, albums_filter=albums_filter, artists_filter=artists_filter)
+        return render_template("results.html", title = title, songs=songs_filtered, albums_filter=sorted(albums_filter), artists_filter=sorted(artists_filter))
 
     else:
         return render_template("index.html")
@@ -86,18 +95,21 @@ def artists():
         title = request.form.get('title')
 
         songs_artists_filtered.clear()
-        albums_filter.clear()
-        artists_filter.clear()
+        albums_filter_filtered.clear()
+        artists_filter_filtered.clear()
 
         for song in songs_filtered:
             if artist == song["artist_name"]:
                 songs_artists_filtered.append(song)
-                albums_filter.append(song["album_name"])
+                albums_filter_filtered.append(song["album_name"])
         
-        artists_filter.append(artist)
+        if artist in artists_filter_filtered:
+            pass
+        else:
+            artists_filter_filtered.append(artist)
 
 
-        return render_template("results.html", title = title, songs = songs_artists_filtered, albums_filter=albums_filter, artists_filter=artists_filter)
+        return render_template("results.html", title = title, songs = songs_artists_filtered, albums_filter=sorted(albums_filter_filtered), artists_filter=sorted(artists_filter_filtered))
 
 
 @app.route("/filter_by_album", methods=["POST"])
@@ -107,14 +119,23 @@ def albums():
         title = request.form.get('title')
 
         songs_albums_filtered.clear()
-        albums_filter.clear()
-        artists_filter.clear()
+        albums_filter_filtered.clear()
+        artists_filter_filtered.clear()
 
         for song in songs_filtered:
             if album == song["album_name"]:
                 songs_albums_filtered.append(song)
-                artists_filter.append(song["artist_name"])
+                artists_filter_filtered.append(song["artist_name"])
         
-        albums_filter.append(album)
+        if album in albums_filter_filtered:
+            pass
+        else:
+            albums_filter_filtered.append(album)
 
-        return render_template("results.html", title = title, songs = songs_albums_filtered, albums_filter=albums_filter, artists_filter=artists_filter)
+        return render_template("results.html", title = title, songs = songs_albums_filtered, albums_filter=sorted(albums_filter_filtered), artists_filter=sorted(artists_filter_filtered))
+
+@app.route("/back")
+def previous():
+    title = request.form.get('title')
+
+    return render_template("results.html", title = title, songs = songs_filtered, albums_filter=sorted(albums_filter), artists_filter=sorted(artists_filter))
